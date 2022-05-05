5c6461b21da23a2c30aa32f762c00bfdd8dd647f
taylan <tylnercan94@gmail.com>
2022-05-05 04:02:58 +0300

    Slider configuration is done
    Edited default controller for getting slider images from database instead of gettin from a static file.
    Changed Ayasofya imagefor fitting it in slider container without distorting it.

--- TravelTripProjectMVC/Controllers/DefaultController.cs
+++ TravelTripProjectMVC/Controllers/DefaultController.cs
@@ -3,15 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TravelTripProjectMVC.Models.Classes;
 
 namespace TravelTripProjectMVC.Controllers
 {
     public class DefaultController : Controller
     {
         // GET: Default
+        Context context = new Context();
+
         public ActionResult Index()
         {
-            return View();
+            var values = context.Blogs.ToList();
+            return View(values);
         }
 
         public ActionResult About()
