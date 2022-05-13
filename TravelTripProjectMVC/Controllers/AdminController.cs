02d9aaeae24dd8a3c1781c05c0bd6bfc22058c9a
taylan <tylnercan94@gmail.com>
2022-05-13 03:33:04 +0300

    Admin panel improvements
    created a dynamic table that contains list of blogs, also some infos like title, date.
    added two actionresult method in admincontroller to handle httpget and httpPost.
    created view for savin/adding new Blog object record to db.

--- TravelTripProjectMVC/Controllers/AdminController.cs
+++ TravelTripProjectMVC/Controllers/AdminController.cs
@@ -3,15 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TravelTripProjectMVC.Models.Classes;
 
 namespace TravelTripProjectMVC.Controllers
 {
     public class AdminController : Controller
     {
         // GET: Admin
+        Context context = new Context();
         public ActionResult Index()
+        {
+            var values = context.Blogs.ToList();
+            return View(values);
+        }
+
+        [HttpGet]
+        public ActionResult NewBlog() 
         {
             return View();
         }
-    }
+
+        [HttpPost]
+        public ActionResult NewBlog(Blog b) 
+        {
+            return View();
+        }
+    }   
+
 }
